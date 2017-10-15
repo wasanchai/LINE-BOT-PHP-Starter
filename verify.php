62d7b8a6290fdfea1e0d7eaf588e1e992bed731d
wasanchai <wasanchai@hotmail.com>
2017-10-15 21:13:44 +0700

    v

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = '7b68170d7c6601b9ce7988d8bf8165b9';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
